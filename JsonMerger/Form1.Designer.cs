102c1ba0a2b10b32bc1e0c36fe59f1009d2cb533
rober <rober@DESKTOP-JD1F31O>
2018-12-17 00:17:21 +0100

    Init

--- JsonMerger/Form1.Designer.cs
+++ JsonMerger/Form1.Designer.cs
@@ -28,12 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.elem = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.TextBox();
+            this.end = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mergeImages = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 140);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(425, 479);
+            this.textBox1.TabIndex = 2;
+            // 
+            // elem
+            // 
+            this.elem.Location = new System.Drawing.Point(201, 114);
+            this.elem.Name = "elem";
+            this.elem.Size = new System.Drawing.Size(72, 20);
+            this.elem.TabIndex = 3;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(279, 114);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(76, 20);
+            this.start.TabIndex = 4;
+            // 
+            // end
+            // 
+            this.end.Location = new System.Drawing.Point(361, 113);
+            this.end.Name = "end";
+            this.end.Size = new System.Drawing.Size(76, 20);
+            this.end.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Element";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(276, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Start";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "End";
+            // 
+            // mergeImages
+            // 
+            this.mergeImages.Location = new System.Drawing.Point(466, 12);
+            this.mergeImages.Name = "mergeImages";
+            this.mergeImages.Size = new System.Drawing.Size(214, 58);
+            this.mergeImages.TabIndex = 9;
+            this.mergeImages.Text = "mergeImages";
+            this.mergeImages.UseVisualStyleBackColor = true;
+            this.mergeImages.Click += new System.EventHandler(this.mergeImages_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 631);
+            this.Controls.Add(this.mergeImages);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.end);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.elem);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox elem;
+        private System.Windows.Forms.TextBox start;
+        private System.Windows.Forms.TextBox end;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button mergeImages;
     }
 }
 
--- JsonMerger/Form1.cs
+++ JsonMerger/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,99 @@ namespace JsonMerger
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Multiselect = true;
+                if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    // Read the files
+
+                    string content = "{";
+
+                    foreach (string file in dlg.FileNames)
+                    {
+
+                        var txt = File.ReadAllText(file);
+                        var name = Path.GetFileNameWithoutExtension(file);
+
+                        if(content != "")
+                        {
+                            content += ",";
+                        }
+
+                        content += "\n" + '\u0022' +name+ '\u0022' + ":" + txt;
+
+
+                    }
+
+                    content += "}";
+
+                    File.WriteAllText(@"d:\videogames.json", content);
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string et = end.Text.Replace(".", ",");
+            string st = start.Text.Replace(".", ",");
+            float stf = float.Parse(st);
+            float etf = float.Parse(et); ;
+            float length = etf - stf;
+  
+            for (int i = 0; i < Math.Round(length / 1.2) + 1; i++)
+            {
+                float val = (float)Math.Round((stf + i * 1.2f) * 10f) / 10f;
+                var txt = "<option value=\"" + val + "\" >" + val + " meter </option>\n";
+                txt = txt.Replace(",", ".");
+                
+                textBox1.AppendText(txt);
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        string[] files;
+        List<Image> images = new List<Image>();
+
+        private void mergeImages_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Multiselect = true;
+                if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    // Read the files
+
+                    files = dlg.FileNames;
+                    foreach (string file in dlg.FileNames)
+                    {
+
+
+
+                        images.Add(Image.FromFile(file));
+
+
+                    }
+
+                }
+            }
+
+
+
         }
     }
 }
